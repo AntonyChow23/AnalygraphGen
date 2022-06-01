c334f7440984b4315d463d2b10bbb0649adbf022
Junwei Zhou <junweizh@andrew.cmu.edu>
2022-05-31 22:49:58 -0400

    pixel by pixel method

--- main.cpp
+++ main.cpp
@@ -26,10 +26,19 @@ using namespace Eigen;
 #define MAX_THREAD 4
 #define MAX_ID 400
 
-Mat Analy;
-Matrix3f M_l;
-Matrix3f M_r;
-int Col_i;
+Mat Analy, Left, Right, M_l, M_r;
+Mat A = (Mat_<float>(3, 3) << 1, 1, 1, 1, 1, 1, 1, 1, 1);
+typedef Point3_<uint8_t> Pixel;
+
+//Matrix3f M_l << 1, 1, 1, 1, 1, 1, 1, 1, 1, 1;
+//int Col_i;
+
+//Mat M_l(3,3,double);
+//Mat M_r = (Mat_<double>(3, 3) << 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+//double M_l[3][3] = {{0,0,0}};
+//double M_r[3][3];
 /*
 for multithreading
 void *multiply(void *arg)
@@ -37,7 +46,7 @@ void *multiply(void *arg)
 
 }
 */
-
+/*
 Mat analy_gen(const Mat &left_graph, const Mat &right_graph, Matrix3f M_l, Matrix3f M_r)
 {
     typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> MatrixXf;
@@ -56,15 +65,19 @@ Mat analy_gen(const Mat &left_graph, const Mat &right_graph, Matrix3f M_l, Matri
     // get eigen matrix
     left = left.reshape(1, 0);
     cv2eigen(left, left_RGB);
-    left_RGB.resize(3, left_RGB.cols() * left_RGB.rows() / 3); // assume 3 channels
+    cout << "left size: " << left_RGB.size() << " " << left_RGB.rows() << " " << left_RGB.cols() << endl;
+    cout << "left: " << left_RGB(0, seqN(0, 10)) << endl;
+    left_RGB = left_RGB.reshaped(3, left_RGB.cols() * left_RGB.rows() / 3).eval(); // assume 3 channels
+    cout << "left: " << left_RGB(0, seqN(0, 10)) << endl;
 
     right = right.reshape(1, 0);
     cv2eigen(right, right_RGB);
-    right_RGB.resize(3, right_RGB.cols() * right_RGB.rows() / 3); // assume 3 channels
+    right_RGB = right_RGB.reshaped(3, right_RGB.cols() * right_RGB.rows() / 3).eval(); // assume 3 channels
 
     // generate analygraph in matrix
     // future multithreading update
     A = M_l * left_RGB;
+    cout << "A: " << A(0, seqN(0, 10)) << endl;
     B = M_r * right_RGB;
     analy_RGB = A + B;
     analy_RGB.resize(Row, analy_RGB.size()/Row);
@@ -76,20 +89,115 @@ Mat analy_gen(const Mat &left_graph, const Mat &right_graph, Matrix3f M_l, Matri
     analy_graph = analy_graph.reshape(n_channels, 0);
     return analy_graph;
 }
-
+*/
 int main(int argc, char **argv)
 {
     int opt;
     int half_width = 0;
     char *image_path = NULL;
+    int i,j;
 
-    Mat left, right, out, img;
+    double number[3] = {0.5, 0.5, 0.5};
 
-    M_l = Matrix3f::Zero();
-    M_r = Matrix3f::Zero();
+    //Mat left, right, out, img;
+    Mat img, img1, img3;
+
+    img = imread("images/pic.jpg", IMREAD_COLOR);
+
+    cout << "img: " << img.size() << " " << img.channels() << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << img.row(0).col(i) << " ";
+    }
+    cout << endl;
+
+    img1 = img.reshape(1,0);
+    cout << "img: " << img1.size() << " " << img1.channels() << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << img1.row(0).col(i) << " ";
+    }
+    cout << endl;
+
+    Mat img2[3];
+    split(img, img2);
+    //cout << "img2: " << img2.size() << " " << img2.channels() << endl;
+    Mat A = img2[0];
+    Mat B = img2[1];
+    Mat C = img2[2];
+    hconcat(A, B, img3);
+    hconcat(img3, C, img3);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << A.row(0).col(i) << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << img3.row(0).col(i) << " ";
+    }
+    cout << endl;
+
+    img1 = img.reshape(3, img.rows*img.cols*img.channels()/3);
+
+    Mat num = (Mat_<float>(3, 3) << 0.3185, 0.0769, 0.0109, 0.1501, 0.0767, 0.0056, 0.0007, 0.0020, 0.0156);
+    Mat num2 = Mat::zeros(img.size(), CV_8UC3);
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < img1.rows; j++)
+        {
+            Pixel pixel = img1.at<Pixel>(0, j) * number[0] + img1.at<Pixel>(1, j) * number[1] + img1.at<Pixel>(2, j) * number[2];
+            //Pixel pixel = img1.at<Pixel>(0, j) * num.row(i).col(0) + img1.at<Pixel>(1, j) * num.row(i).col(1) + img1.at<Pixel>(2, j) * num.row(i).col(2);
+            num2.at<Pixel>(i,j) = pixel;
+        }
+    }
+    imshow("num2", num2);
+    waitKey(0);
+    destroyAllWindows();
+    //Mat num1 = num * img1;
+
+    //Mat num2;
+    //num2 = img.reshape(3, 0);
+
+    //Mat out = 
+
+    
+    /*for (i=0; i < A.rows; i++)
+    {
+        for (j = 0; j < A.cols; j++)
+        {
+            Pixel px = A.at<Pixel>(i, j);
+            cout << px << " ";
+            // cout << A.row(i).col(j);
+        }
+        cout << endl;
+    }*/
+
+    //M_l[0] = {0, 0, 0};
+
+    //int a[4] = {0, 0, 0, 0};
+    //int x[3][2] = {1,2,3,4,5,6};
+    //x[3][2] = {0,0,0,0,0,0};
+
+    //Mat A = (Mat_<float>(3, 3) << 1, 1, 1, 1, 1, 1, 1, 1, 1);
+    //Mat C[3];
+    //split(A, C);
+    //cout << C << endl;
+    //Mat A[3];
+    //A = (Mat_<float>(3, 3) << 1, 1, 1, 1, 1, 1, 1, 1, 1);
+    //<< 1, 1, 1, 1, 1, 1, 1, 1, 1;
+
+    //A[0] = {1,2,3};
+    //M_l = Matrix3f::Zero();
+    //M_r = Matrix3f::Zero();
     //Matrix3f M_l = Matrix3f::Zero();
     //Matrix3f M_r = Matrix3f::Zero();
 
+    //M_l = {}
+    /*
     while ((opt = getopt(argc, argv, "l:r:i:TGCHODR")) != -1)
     {
         switch (opt)
@@ -104,6 +212,12 @@ int main(int argc, char **argv)
                     cout << "Error loading image." << endl;
                     return -1;
                 }
+
+                Mat B[3];
+                split(left, B);
+                Mat D = B.mul(C);
+                //float C = A.col(0)*B[0];
+
                 cvtColor(left, left, COLOR_BGR2RGB);
 
                 break;
@@ -112,13 +226,13 @@ int main(int argc, char **argv)
                 memcpy(image_path, optarg, strlen(optarg) + 1);
 
                 right = imread(image_path, IMREAD_COLOR);
-                if (!left.data)
+                if (!right.data)
                 {
                     cout << "Error loading image." << endl;
                     return -1;
                 }
 
-                cvtColor(left, left, COLOR_BGR2RGB);
+                cvtColor(right, right, COLOR_BGR2RGB);
                 break;
             case 'i':
                 image_path = (char *)malloc(strlen(optarg) + 1);
@@ -137,10 +251,18 @@ int main(int argc, char **argv)
                 right = img(Range(0, img.size().height), Range(half_width, img.size().width));
 
                 break;
+            
             case 'T': // True analygraph
-                M_l.row(0) << 0.299, 0.587, 0.114;
-                M_r.row(2) << 0.299, 0.587, 0.114;
+                //(Mat_<double>(3, 3) << 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                //M_l.row(0) << 0.299, 0.587, 0.114;
+                //M_l.row(0) = {0.299, 0.587, 0.114};
+                //M_r.row(2) = (Mat_<double>(1, 3) << 0.299, 0.587, 0.114);
+
+                cout << M_l << endl;
+                cout << M_r << endl;
+
                 break;
+            
             case 'G': // Gray analygraph
                 M_l.row(0) << 0.299, 0.587, 0.114;
                 M_r.row(1) << 0.299, 0.587, 0.114;
@@ -171,17 +293,23 @@ int main(int argc, char **argv)
                 M_l << 0.3185, 0.0769, 0.0109, 0.1501, 0.0767, 0.0056, 0.0007, 0.0020, 0.0156;
                 M_r << 0.0174, 0.0484, 0.1402, 0.0184, 0.1807, 0.0458, 0.0286, 0.0991, 0.7662;
                 break;
+            
             default:
                 cout << "Unknown option character " << endl;
                 return -1;
         }
     }
-
-    out = analy_gen(left, right, M_l, M_r);
+    */
+    //out = analy_gen(left, right, M_l, M_r);
+    /*
+    imshow("RGB", out / 255.0);
+    cvtColor(out, out, COLOR_RGB2BGRA);
 
     imwrite("analygraph.jpg", out);
-    imshow("out", out / 255.0);
+    imshow("BGR", out / 255.0);
     waitKey(0);
     destroyAllWindows();
+
+    */
     return 0;
 }
